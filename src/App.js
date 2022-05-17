1421364a29ec9b290e4154f730a1bf2a19ac8e14
ot07 <ot07neoki@gmail.com>
2022-05-17 23:09:15 +0900

    play sine wave

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
 import * as Tone from "tone";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    isPlaying ? Tone.Transport.start() : Tone.Transport.stop();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const osc = new Tone.Oscillator().toDestination();
+    // repeated event every 8th note
+    Tone.Transport.scheduleRepeat((time) => {
+      // use the callback time to schedule events
+      osc.start(time).stop(time + 0.1);
+    }, "8n");
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello Tone.js</h1>
+      <button onClick={() => setIsPlaying((isPlaying) => !isPlaying)}>
+        {isPlaying ? "stop" : "start"}
+      </button>
     </div>
   );
 }
